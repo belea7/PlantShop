66bb2017152be3c1f5eb5d9fe2f3260cf7d2a940
RonMosenzon <50575801+RonMosenzon@users.noreply.github.com>
2021-03-17 12:59:34 +0200

    Update UserBean.java

--- src/main/java/PlantShop/beans/UserBean.java
+++ src/main/java/PlantShop/beans/UserBean.java
@@ -2,6 +2,9 @@ package PlantShop.beans;
 
 import PlantShop.daos.UserDao;
 import PlantShop.entities.User;
+import PlantShop.exceptions.DaoException;
+import PlantShop.exceptions.IncorrectCredentialsException;
+import PlantShop.exceptions.UsernameTakenException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -33,35 +36,37 @@ public class UserBean implements Serializable {
     
     /**
      * Attempts to log in to the account with the specified username and password.
-     * @return true if login was successful, false otherwise.
+     * @throws DaoException if there was a problem with database access.
+     * @throws IncorrectCredentialsException if the username or password are incorrect.
      */
-    public boolean login(String userName, String password) {
+    public void login(String userName, String password)
+            throws DaoException, IncorrectCredentialsException {
         
         User user = userDao.getUser(userName);
-        if(user == null)
-            return false; // user doesn't exist
         
+        if(user == null)
+            throw new IncorrectCredentialsException();
         if(password.compareTo(user.getPassword()) != 0)
-            return false; // wrong password
+            throw new IncorrectCredentialsException();
         
         this.user = user;
-        return true;
     }
     
     
     /**
      * Attempts to register a new user with the specified information.
      * @return true if registration was successful, false otherwise.
+     * @throws DaoException if there was a problem with database access.
+     * @throws UsernameTakenException if the username is already taken.
      */
-    public boolean register(User user) {
+    public void register(User user)
+            throws DaoException, UsernameTakenException {
         
         if(userDao.getUser(user.getUsername()) != null)
-            return false; // username already taken
+            throw new UsernameTakenException(); // username already taken
         
         userDao.addUser(user);
         this.user = user;
-        return true;
-        
     }
     
     
