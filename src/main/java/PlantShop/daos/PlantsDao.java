ea322b7f87bc5f0260130039685826bab0beb6c3
RonMosenzon <50575801+RonMosenzon@users.noreply.github.com>
2021-03-31 12:38:03 +0300

    Update PlantsDao.java

--- src/main/java/PlantShop/daos/PlantsDao.java
+++ src/main/java/PlantShop/daos/PlantsDao.java
@@ -21,10 +21,10 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 
 @DataSourceDefinition(
-    name = "java:global/jdbc/PlantShop",
+    name = "java:global/jdbc/plantShop",
     className = "org.apache.derby.jdbc.ClientDataSource",
-    url = "jdbc:derby://localhost:1527/PlantShop",
-    databaseName = "PlantShop",
+    url = "jdbc:derby://localhost:1527/plantShop",
+    databaseName = "plantShop",
     user = "root",
     password = "root")
 
@@ -42,7 +42,7 @@ public class PlantsDao implements Serializable{
     private ReviewsDao reviewsDao;               // Reviews DAO
     
     // Allow the server to inject the DataSource
-    @Resource(lookup="java:global/jdbc/PlantShop")
+    @Resource(lookup="java:global/jdbc/plantShop")
     DataSource dataSource;
     
     /**
