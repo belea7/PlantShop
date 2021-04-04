f717418db3d57f88b03499947ae2c1de359f253c
Lea Ben Zvi <leagirsh@gmail.com>
2021-04-04 18:42:48 +0300

    Update EditProfileController.java

--- src/main/java/PlantShop/controller/EditProfileController.java
+++ src/main/java/PlantShop/controller/EditProfileController.java
@@ -1,12 +1,10 @@
 package PlantShop.controller;
 
-import PlantShop.entities.ShoppingCart;
 import PlantShop.entities.User;
 import PlantShop.exceptions.DaoException;
 import PlantShop.exceptions.ProfileInputFormatException;
 import PlantShop.exceptions.UsernameTakenException;
 import PlantShop.model.UserModel;
-import PlantShop.util.ErrorDisplay;
 import PlantShop.view.MessagesView;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
@@ -49,7 +47,7 @@ public class EditProfileController extends AbstractProfileDataInputController {
         this.setUsername(userModel.getUser().getUsername());
         this.setFirstName(userModel.getUser().getFirstName());
         this.setLastName(userModel.getUser().getLastName());
-        this.setBirthDate(String.valueOf(userModel.getUser().getBirthDate()));
+        this.setBirthdayDate(String.valueOf(userModel.getUser().getBirthdayDate()));
         this.setEmailAddress(userModel.getUser().getEmailAddress());
         this.setPhoneNumber(userModel.getUser().getPhoneNumber());
         this.setCity(userModel.getUser().getCity());
