3dcc5d396b9eca083fedf86b58bfe6b86c50e48e
RonMosenzon <50575801+RonMosenzon@users.noreply.github.com>
2021-04-08 17:57:47 +0300

    made checkOut update plants in stock

--- src/main/java/PlantShop/controller/ShoppingCartController.java
+++ src/main/java/PlantShop/controller/ShoppingCartController.java
@@ -4,6 +4,8 @@
 package PlantShop.controller;
 
 import PlantShop.daos.OrdersDao;
+import PlantShop.daos.PlantsDao;
+import PlantShop.daos.ShoppingCartDao;
 import PlantShop.entities.Order;
 import PlantShop.entities.Plant;
 import PlantShop.entities.PlantInCart;
@@ -39,7 +41,13 @@ public class ShoppingCartController implements Serializable{
     private MessagesView messagesView;
     
     @Inject
+    private ShoppingCartDao shoppingCartDao;
+    
+    @Inject
     private OrdersDao ordersDao;
+    
+    @Inject
+    private PlantsDao plantsDao;
 
     /**
      * Updated and returns plants in cart from shopping cart model.
@@ -245,6 +253,28 @@ public class ShoppingCartController implements Serializable{
                 messagesView.displayErrorMessage("Failed to remove plant from cart");
                 return;
             }
+        }
+        
+        // update plant counts in stock
+        int newCount;
+        for(Order.PlantInOrder plant : plantsInOrder) {
+            newCount = Math.max(0, plant.getPlant().getNumberOfItems() - plant.getAmount());
+            plant.getPlant().setNumberOfItems(newCount);
+            try{
+                plantsDao.updatePlant(plant.getPlant());
+            } catch(DaoException e) {
+                e.printStackTrace();
+                messagesView.displayErrorMessage("Failed to update plant count in stock");
+                return;
+            }
+        }
+        
+        // update other carts that no longer have items available in stock
+        try{
+            shoppingCartDao.updateAllCarts();
+        } catch(DaoException e) {
+            e.printStackTrace();
+            messagesView.displayErrorMessage("Failed to update all carts for new plant count in stock");
         }
     }
 }
